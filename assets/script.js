e0ac38d43e38341d0dc1949e52c3d43cfb000f00
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-01-17 15:19:35 -0500

    begin javascript

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//Start Section
+let start = document.querySelector("#start");
+
+//guide Section
+let guide = document.querySelector("#guide");
+let exit = document.querySelector("#exit");
+let continueBtn = document.querySelector("#continue");
