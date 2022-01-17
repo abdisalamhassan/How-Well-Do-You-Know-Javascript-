c654384c3590edc21f63e4870148bf1cfc8b807e
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-01-17 16:02:55 -0500

    add timer

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,44 @@
-//Start Section
-let start = document.querySelector("#start");
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
-//guide Section
-let guide = document.querySelector("#guide");
-let exit = document.querySelector("#exit");
-let continueBtn = document.querySelector("#continue");
+//what happen when 'Start' Button Will Click
+start.addEventListener("click", () => {
+    start.style.display = "none";
+    guide.style.display = "block";
+});
+
+
+/what happen when 'Exit' Button Will Click
+exit.addEventListener("click", () => {
+    start.style.display = "block";
+    guide.style.display = "none";
+});
+
+
+//Creating Timer For Quiz Timer Section
+
+let countDown = () => {
+    if (timer === 20) {
+        clearInterval(interval);
+        next_question.click();
+    } else {
+        timer++;
+        time.innerText = timer;
+    }
+}
+
+//setInterval(countDown,1000);
+
+let loadData = () => {
+    questionNo.innerText = index + 1 + ". ";
+    questionText.innerText = MCQS[index].question;
+    option1.innerText = MCQS[index].choice1;
+    option2.innerText = MCQS[index].choice2;
+    option3.innerText = MCQS[index].choice3;
+    option4.innerText = MCQS[index].choice4;
+
+    //    timer start
+    timer = 0;
+}
+
